0c1161aa3fefc0de66457c1ca94fbc351574efbe
ANIPLEX <i@cherry.moe>
2016-12-10 17:20:23 +0800

    TwoSum

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -1,4 +1,8 @@
 package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /*
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
  * You may assume that each input would have exactly one solution.
@@ -7,8 +11,28 @@ package leetcode;
  * return [0, 1].
  */
 public class TwoSum {
-    public static void main(String[] args) {
+    public int[] twoSum(int[] nums, int target) {
 
+        if (nums == null || nums.length <= 1) {
+            return new int[2];
+        }
 
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        // key = target - nums[i], just one solution
+        for (int i = 0; i < nums.length; i++) {
+            map.put(target - nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer v = map.get(nums[i]);
+
+            // can't use itself
+            if (v != null && v != i) {
+                return new int[] { i + 1, v + 1 };
+            }
+        }
+
+        return null;
     }
 }
