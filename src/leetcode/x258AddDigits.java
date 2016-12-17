a8520ce5a34a9c324382b1d8577b5cf1e03db907
ANIPLEX <i@cherry.moe>
2016-12-17 20:50:42 +0800

    258

---
+++ src/leetcode/x258AddDigits.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+/**
+ * Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+ *For example:
+ *Given num = 38, the process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+ */
+public class x258AddDigits {
+    public int addDigits(int num) {
+        while(num>=10){
+            num = (num/10)+num%10;
+        }
+        return num;
+    }
+
+}
